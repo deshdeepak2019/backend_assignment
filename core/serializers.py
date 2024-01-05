3f29a89e430dd7ff30e14bcdd65d1b0bc934c528
DESH DEEPAK DHRUV <deshdeepak.btech.iet@gmail.com>
2024-01-05 20:36:48 +0530

    modify serializer

--- core/serializers.py
+++ core/serializers.py
@@ -6,4 +6,4 @@ from .models import Animal
 class AnimalSerializer(serializers.ModelSerializer):  # type: ignore[type-arg]
     class Meta:
         model = Animal
-        fields = "__all__"
+        fields = ["id", "name", "age", "type", "sound", "color"]
