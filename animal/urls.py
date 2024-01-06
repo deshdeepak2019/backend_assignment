af3a24cdf40ed6dc87d601bb6941ff44878b13a9
DESH DEEPAK DHRUV <deshdeepak.btech.iet@gmail.com>
2024-01-06 12:26:40 +0530

    Token generated using post save signal

--- animal/urls.py
+++ animal/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from core.viewsets.token import CustomAuthToken
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
-    path("api-token-auth/", CustomAuthToken.as_view()),
 ]
